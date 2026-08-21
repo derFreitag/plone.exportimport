f1e5839d55f0bb44a5656cd3d6a57054a76f587a
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-08-21 11:31:22 +0200

    Back to development: 2.0.1.post1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = f"""
 
 setup(
     name="plone.exportimport",
-    version="2.0.1.post0",
+    version="2.0.1.post1.dev0",
     description="Plone content export / import support",
     long_description=long_description,
     long_description_content_type="text/markdown",
